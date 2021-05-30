1ec061ac7acdc093105fc668b76f490947f87791
name <email>
2021-05-30 18:14:13 +0800

    优化

--- App.go
+++ App.go
@@ -11,11 +11,16 @@ import (
 
 //https://docs.docker.com/engine/api/sdk/examples/
 
-func main() {
-	fmt.Println("Start Params:")
+func PrintParamInfo() {
+
+	fmt.Println("Parameter description:")
 	fmt.Println("\tDockerServer\trequired\thttp server address，like：http://127.0.0.1:8080/dockerApi")
 	fmt.Println("\tDockerWsServer\trequired\twebsocket server address，like：http://127.0.0.1:8080/dockerApi/ws")
 	fmt.Println("\tToken\t\tOptional\tThe http and websocket header authorization for dockerserver auth")
+}
+
+func main() {
+	PrintParamInfo()
 
 	flag.StringVar(&conf.DockerServer, "DockerServer", "http://127.0.0.1:8080/dockerApi", "DockerServer服务地址，如：http://127.0.0.1:8080/dockerApi")
 	flag.StringVar(&conf.DockerWsServer, "DockerWsServer", "", "DockerWsServer服务地址，如：http://127.0.0.1:8068/dockerApi/ws")
@@ -26,15 +31,15 @@ func main() {
 	log.Println("conf.DockerServer:", conf.DockerServer)
 	log.Println("conf.DockerWsServer:", conf.DockerWsServer)
 
-	if conf.DockerServer == "" && conf.DockerWsServer == "" {
-		log.Panic("DockerServer and DockerWsServer, must one of not empty\nlike: -DockerServer http://127.0.0.1:8080/dockerApi, or  -DockerWsServer http://127.0.0.1:8080/dockerApi/ws ")
+	if conf.DockerServer == "" {
+		log.Panic("DockerServer must be set. like: -DockerServer http://127.0.0.1:8080/dockerApi")
 	}
 
 	if conf.DockerWsServer != "" {
 		log.Println("Start connect to DockerWsServer :", conf.DockerWsServer)
 		agent.StartWs()
 	} else {
-		log.Println("DockerWsServer not set, that websocket client not start")
+		log.Println("DockerWsServer not set, that websocket client not start, if you want websocket, you can add param,like :  -DockerWsServer http://127.0.0.1:8080/dockerApi/ws ")
 	}
 
 	for true {
